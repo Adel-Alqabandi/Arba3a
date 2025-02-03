afa3a75ae6a8125a4ce7f890790acb0ad0cabf7b
Adel-Alqabandi <uceelqa@ucl.ac.uk>
2025-02-03 21:51:12 +0000

    Datetime processing
    Added a new column FL_DATETIME that shows the departure date and time in the format (dd/mm/yyyy hh:mm).
    
    Removed FL_DATE and CRS_DEP_TIME columns that are not required anymore.

--- departure_datasets.py
+++ departure_datasets.py
@@ -1,7 +1,13 @@
-import pandas as pd
-import numpy as np
+######################################################################
+# function that returns pickle dictionary of top 10 airport departure datasets
 
-# function that return dictionary of top 10 airport departure datasets
+# to select dataset of an airport, use the airport code as the key in the dict datasets
+# e.g. datasets["LAX"]
+#####################################################################
+
+import pandas as pd
+import pickle
+
 def departure_datasets():
     # read .csv files and append into one big dataset
     df_appended = []
@@ -15,6 +21,26 @@ def departure_datasets():
 
     dep_data = pd.concat(df_appended, ignore_index=True)
 
+    # replacing the record time from FL_DATE with the planned departure date
+    # convert FL_DATE column into datetime object 
+    dep_data["FL_DATE"] = pd.to_datetime(dep_data["FL_DATE"], format="%m/%d/%Y %I:%M:%S %p")
+
+    # add colon to seperate hours and minutes
+    dep_data["CRS_DEP_TIME"] = dep_data["CRS_DEP_TIME"].astype(str).str.zfill(4)  #"530" becomes "0530"
+    dep_data["CRS_DEP_TIME"] = dep_data["CRS_DEP_TIME"].str[:2] + ":" + dep_data["CRS_DEP_TIME"].str[2:]
+
+    # some records incorrectly interpret 00:00 as 24:00
+    dep_data["CRS_DEP_TIME"] = dep_data["CRS_DEP_TIME"].replace("24:00", "00:00")
+
+    # convert CRS_DEP_TIME into datetime object
+    dep_data["CRS_DEP_TIME"] = pd.to_datetime(dep_data["CRS_DEP_TIME"], format="%H:%M")
+
+    # taking only the date from FL_DATE and combining with time from CRS_DEP_TIME
+    dep_data["FL_DATETIME"] = (dep_data["FL_DATE"].dt.strftime("%d/%m/%Y") + " " + dep_data["CRS_DEP_TIME"].dt.strftime("%H:%M"))
+
+    # removing irrelevant columns
+    dep_data = dep_data.drop(columns=["FL_DATE", "CRS_DEP_TIME"])
+
     # only keep entries of airports in list of 10 busiest airports
     airports_top10 = ["ATL", "DFW", "DEN", "LAX", "ORD", "JFK", "MCO", "LAS", "CLT", "MIA"]
 
@@ -27,7 +53,9 @@ def departure_datasets():
     for n in airports_top10:
         datasets[n] = dep_data_top10.loc[dep_data_top10["ORIGIN"]==n].reset_index(drop=True)
     
-    return datasets
+    # writing datasets to pickle file
+    with open("flight_datasets.pkl", "wb") as file:
+        pickle.dump(datasets, file)
 
-#datasets = departure_datasets()
-#print(datasets["ATL"])
+# running the function
+#departure_datasets()
